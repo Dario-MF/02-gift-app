a84c33addfe60f9e03ef4ec326b6314a828dd47f
Dario MF <ferrada_86@hotmail.com>
2021-02-28 05:49:07 +0100

    add test gridItem

--- src/Components/GridItem.js
+++ src/Components/GridItem.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const GridItem = ({ url, title }) => {
 
     return (
-        <div className={'card'}>
+        <div className={'card card_item'}>
             <img src={url} alt={title} />
             <p>{title}</p>
         </div>
     )
+}
+
+GridItem.propTypes = {
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
 }
 
 export default GridItem
---
+++ src/Tests/GridItem.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { shallow } from "enzyme"
+import GridItem from "../Components/GridItem"
+
+describe('tests for <GridItem />', () => {
+    const title = 'titulo'
+    const url = 'https//:localhost/image.jpg'
+    const wrapped = shallow(<GridItem title={title} url={url} />)
+
+    test('snapshot <GridItem />', () => {
+
+
+        expect(wrapped).toMatchSnapshot()
+    })
+
+    test('comprobar render de p', () => {
+        const p = wrapped.find('p')
+
+        expect(p.text().trim()).toBe(title)
+    })
+
+    test('comprobar que img recibe props correctamente', () => {
+        const img = wrapped.find('img')
+
+        expect(img.prop('src')).toBe(url)
+        expect(img.prop('alt')).toBe(title)
+    })
+
+    test('comprobar className de div', () => {
+        const divClassName = wrapped.find('div').prop('className')
+
+        expect(divClassName.includes('card_item')).toBe(true)
+
+    })
+
+
+
+
+})
